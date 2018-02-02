79a1af1034ed7bf6f4492146329e4494c99e49b4
lennedy <lennedy@gmail.com>
2018-02-02 00:26:12 -0300

    Mudança de tipo recebido para ficar de acordo com o tipo CalibracaoDataConfig que o tipo utilizado na comunicação.

--- node/cali_server.cpp
+++ node/cali_server.cpp
@@ -1,20 +1,21 @@
 #include <ros/ros.h>
 
 #include <dynamic_reconfigure/server.h>
+#include <visao_obr/CalibracaoDataConfig.h>
 
-void callback(dynamic_tutorials::TutorialsConfig &config, uint32_t level) {
-//  ROS_INFO("Reconfigure Request: %d %d %d %s %d", 
-//            config.cali_rect, config.cali_circ_canny, 
-//            config.cali_circ_hough, 
-//            config.bool_param?"True":"False", 
-//            config.size);
+void callback(visao_obr::CalibracaoDataConfig &config, uint32_t level) {
+   ROS_INFO("Reconfigure Request: %d %d %d %s %d", 
+            config.cali_rect, config.cali_circ_canny, 
+            config.cali_circ_hough, 
+            config.bool_param?"True":"False", 
+            config.size);
 }
 
 int main(int argc, char **argv) {
   ros::init(argc, argv, "CalibrationServer");
 
-  dynamic_reconfigure::Server<dynamic_tutorials::TutorialsConfig> server;
-  dynamic_reconfigure::Server<dynamic_tutorials::TutorialsConfig>::CallbackType f;
+  dynamic_reconfigure::Server<visao_obr::CalibracaoDataConfig> server;
+  dynamic_reconfigure::Server<visao_obr::CalibracaoDataConfig>::CallbackType f;
 
   f = boost::bind(&callback, _1, _2);
   server.setCallback(f);
